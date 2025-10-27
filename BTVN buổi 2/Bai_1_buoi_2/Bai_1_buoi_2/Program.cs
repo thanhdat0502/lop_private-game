d44fc2eb6be3a75ab48bdc746c608a8a898f6e52
thanhdat <datmai05022006@gmail.com>
2025-10-27 10:34:01 +0700

    BTVN buoi 2

---
+++ BTVN buổi 2/Bai_1_buoi_2/Bai_1_buoi_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Bai_1_buoi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a + b > c || a + c > b || b + c > a)
+            {
+                if (a == b && a == c && b == c) { Console.WriteLine("Tam giac deu"); }
+                else
+                {
+                    if (a == b || b == c || a == c) Console.WriteLine("Tam giac can");
+                    else Console.WriteLine("Tam giac thuong");
+                }
+            }
+            else Console.WriteLine("Khong the tao thanh tam giac");
+        }
+    }
+}
+4
---
+++ BTVN buổi 2/Bai_3_buoi_3/Bai_3_buoi_3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Bai_3_buoi_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j);
+                for (int j = i - 1; j >= 1; j--)
+                    Console.Write(j);
+                Console.WriteLine();
+            }
+        }
+    }
+}
