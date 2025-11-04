c136d1f5e55e614d9f55a1976c2055a1c9b89125
thanhdat <datmai05022006@gmail.com>
2025-11-04 21:22:11 +0700

    BTVN buoi 3

---
+++ BTVN buổi 3/Bài 1/Bài 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            if (Check(s))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static bool Check(string s)
+        {
+            Stack<char> stack1 = new Stack<char>();
+
+            foreach (char i in s)
+            {
+                if (i == '(' || i == '[' || i == '{')
+                {
+                    stack1.Push(i);
+                }
+                else if (i == ')' || i == ']' || i == '}')
+                {
+                    if (stack1.Count == 0) return false;
+
+                    char top = stack1.Pop();
+                    if (!IsMatching(top, i)) return false;
+                }
+            }
+
+            return stack1.Count == 0;
+        }
+
+        static bool IsMatching(char open, char close)
+        {
+            return (open == '(' && close == ')') ||
+                   (open == '[' && close == ']') ||
+                   (open == '{' && close == '}');
+        }
+    }
+}
---
+++ BTVN buổi 3/Bài 2/Bài 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Bài2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Stack<char> s1 = new Stack<char>();
+            foreach (char c in s)
+            {
+                s1.Push(c);
+            }
+            while (s1.Count > 0)
+            {
+                Console.Write(s1.Pop());
+            }
+
+        }
+    }
+}
